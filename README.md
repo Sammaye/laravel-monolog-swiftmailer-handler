652f474083b9a74ca880fbe17edc734850e5730a
Sam Millman <sam.millman@googlemail.com>
2019-04-07 21:11:04 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # laravel-monolog-swiftmailer-handler
-A handler to bridge Laravel, Monolog and Swiftmailer
+
+A handler to bridge Laravel, Monolog and Swiftmailer.
+
+To use it simply include it as a log channel:
+
+```php
+'email' => [
+    'driver' => 'custom',
+    'via' => \sammaye\MonologSwiftMailerHandler\Handler::class,
+    'from' => [
+        'address' => env('MAIL_LOG_FROM_ADDRESS', 'hello@example.com'),
+        'name' => env('MAIL_LOG_FROM_NAME', 'Example'),
+    ],
+    'to' => env('MAIL_LOG_EMAIL_ADDRESS', 'hello@example.com'),
+],
+```
+
+As simple as that.
---
+++ src/Handler.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace sammaye\MonologSwiftMailerHandler;
+
+use Illuminate\Log\ParsesLogConfiguration;
+use Monolog\Formatter\HtmlFormatter;
+use Monolog\Handler\SwiftMailerHandler;
+use Monolog\Logger;
+
+class Handler
+{
+    protected $app;
+
+    use ParsesLogConfiguration;
+
+    protected function getFallbackChannelName()
+    {
+        return $this->app->bound('env') ? $this->app->environment() : 'production';
+    }
+
+    public function __invoke(array $config)
+    {
+        $this->app = app();
+
+        $mailer = app('mailer')->getSwiftMailer();
+        $message = $mailer->createMessage()
+            ->setFrom(
+                $config['from']['address'],
+                $config['from']['name']
+            )
+            ->setTo($config['to'])
+            ->setContentType('text/html');
+
+        $logger = new Logger(
+            'email',
+            [
+                (new SwiftMailerHandler(
+                    $mailer,
+                    $message,
+                    $this->level($config),
+                    $config['bubble'] ?? true
+                ))->setFormatter(new HtmlFormatter())
+            ]
+        );
+
+        return $logger;
+    }
+}
